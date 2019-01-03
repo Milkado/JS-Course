5648686c4e0b667987e226e5cdd773032c4741f5
Lucas de Freitas <lucasdef.miranda@gmail.com>
2019-01-03 01:20:30 -0200

    starting at js

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,86 @@
-console.log('Hello World!!!')
+/*************
+* Variables and data types
+*/
+
+/*
+var firstName = 'Jorg';
+console.log(firstName);
+var lastName = 'Ancrath';
+var age = 14;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//variable name rules
+var 3years = 3;
+*/
+
+/**************************************************
+ * Variable mutation and type coercion
+ */
+
+/* var firstName = 'Jorg';
+ var age = 14;
+
+ //Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+
+job = 'Assassin';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+
+age = 'eighteen';
+job = 'King';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName); */
+
+/*********************
+ * Basic operators
+ */
+var now, yearJorg, yearMakin, ageJorg, ageMakin; 
+now = 2019;
+ageJorg = 18;
+ageMakin = 27;
+
+// Math operators
+yearJorg = now - ageJorg;
+yearMakin = now - ageMakin;
+console.log(yearMakin);
+console.log(yearJorg);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+// Logical operators
+
+var jorgOlder = ageJorg > ageMakin;
+console.log(jorgOlder);
+
+//Typeof operator
+
+console.log(typeof jorgOlder);
+console.log(typeof ageJorg);
+console.log(typeof 'Makin is older than Jorg');
+var x;
+console.log(typeof x);
+
+
+
+
+
+
+
